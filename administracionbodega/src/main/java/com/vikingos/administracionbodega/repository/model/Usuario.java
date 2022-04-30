c5855c231d01c419e9c9b7a2d3fd3cb1c6cb365c
JaimeRG <=>
2022-04-30 11:43:46 -0400

    avance jaime

---
+++ administracionbodega/src/main/java/com/vikingos/administracionbodega/repository/model/Usuario.java
@@ -0,0 +1,39 @@
+package com.vikingos.administracionbodega.repository.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@SequenceGenerator(name = "usuario_id_seq", initialValue = 1, sequenceName = "usuario_id_seq", allocationSize = 1)
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "usuario_id_seq")
+	@Column(name = "id_usuario")
+	private int idUsuario;
+	private String nombre;
+	private String email;
+	private String password;
+	@ManyToOne
+	@JoinColumn(name = "rol_id")
+	private Rol rol;
+	
+	@Override
+	public String toString() {
+		return "Usuario [idUsuario=" + idUsuario + ", nombre=" + nombre + ", email=" + email + ", password=" + password
+				+ ", rol=" + rol.getDescripcion() + "]";
+	}
+	
+}
