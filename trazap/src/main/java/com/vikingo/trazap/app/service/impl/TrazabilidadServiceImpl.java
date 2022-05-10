e18dd716e28b3ea747f2a6472e3b6a6bd346e99a
Pedro Gutierrez <pedropg.rojas@gmail.com>
2022-05-10 18:06:10 -0400

    enviar

--- trazap/src/main/java/com/vikingo/trazap/app/service/impl/TrazabilidadServiceImpl.java
+++ trazap/src/main/java/com/vikingo/trazap/app/service/impl/TrazabilidadServiceImpl.java
@@ -21,10 +21,8 @@ import com.vikingo.trazap.app.service.response.ResponseServiceMessageType;
 import com.vikingo.trazap.app.service.response.ResponseServiceObject;
 import com.vikingo.trazap.app.service.response.producto.TrazabilidadResponse;
 
-
-
 @Service("trazabilidadService")
-public class TrazabilidadServiceImpl implements TrazabilidadService{
+public class TrazabilidadServiceImpl implements TrazabilidadService {
 
 	@Autowired
 	private TrazabilidadRepository trazabilidadRepository;
@@ -36,19 +34,19 @@ public class TrazabilidadServiceImpl implements TrazabilidadService{
 	private ResponseServiceObject responseServiceObject;
 	@Autowired
 	private ResponseServiceMessage responseServiceMessage;
-	
+
 	@Override
 	public ResponseServiceObject findAll() {
 		List<ResponseServiceMessage> messageList = new ArrayList<ResponseServiceMessage>();
-		
+
 		List<Trazabilidad> trazabilidades = new ArrayList<Trazabilidad>();
 		Iterable<Trazabilidad> iterableTrazabilidades = trazabilidadRepository.findAll();
-		
+
 		iterableTrazabilidades.forEach(trazabilidades::add);
-		
-		List<TrazabilidadResponse> response = new ArrayList<TrazabilidadResponse>();	
-		
-		for(Trazabilidad trazabilidad: trazabilidades) {
+
+		List<TrazabilidadResponse> response = new ArrayList<TrazabilidadResponse>();
+
+		for (Trazabilidad trazabilidad : trazabilidades) {
 			TrazabilidadResponse trazabilidadResponse = new TrazabilidadResponse();
 			trazabilidadResponse.setIdTrazabilidad(trazabilidad.getIdTrazabilidad());
 			trazabilidadResponse.setFechaInicioPreparacion(trazabilidad.getFechaInicioPreparacion());
@@ -59,63 +57,65 @@ public class TrazabilidadServiceImpl implements TrazabilidadService{
 			trazabilidadResponse.setPedidos(trazabilidad.getPedidos());
 			trazabilidadResponse.setEstadoTrazabilidad(trazabilidad.getEstadoTrazabilidad());
 			response.add(trazabilidadResponse);
-			
+
 		}
-		
+
 		responseServiceMessage.setTimestamp(new Date());
 		responseServiceMessage.setCode("200");
 		responseServiceMessage.setType(ResponseServiceMessageType.OK);
 		responseServiceMessage.setMessage("Servicio ha finalizado correctamente");
-		
+
 		messageList.add(responseServiceMessage);
-		
+
 		responseServiceObject.setBody(response);
 		responseServiceObject.setMessageList(messageList);
-		
-	return responseServiceObject;
-		
+
+		return responseServiceObject;
+
 	}
-	
+
 	@Override
 	public ResponseServiceObject save(TrazabilidadRequest trazabilidadRequest) {
-		
+
 		List<ResponseServiceMessage> messageList = new ArrayList<ResponseServiceMessage>();
 		Trazabilidad trazabilidad = new Trazabilidad();
 		trazabilidad.setCodigoTrazabilidad(trazabilidadRequest.getCodigoTrazabilidad());
 		trazabilidad.setFechaEnvio(trazabilidadRequest.getFechaEnvio());
-		trazabilidad.setFechaEstimadaEnvio(trazabilidadRequest.getFechaEstimadaEnvio());;
+		trazabilidad.setFechaEstimadaEnvio(trazabilidadRequest.getFechaEstimadaEnvio());
+		;
 		trazabilidad.setFechaFinPreparacion(trazabilidadRequest.getFechaFinPreparacion());
 		trazabilidad.setFechaInicioPreparacion(trazabilidadRequest.getFechaInicioPreparacion());
-		
+
 		List<Pedidos> pedido = new ArrayList<Pedidos>();
 		Iterable<Pedidos> iterablePedidos = pedidosRepository.findAll();
 		iterablePedidos.forEach(pedido::add);
-		
-		for(Pedidos pedidos : pedido) { 
-			if(pedidos.getIdPedido() == trazabilidadRequest.getPedidos().getIdPedido()) {
+
+		for (Pedidos pedidos : pedido) {
+			if (pedidos.getIdPedido() == trazabilidadRequest.getPedidos().getIdPedido()) {
 				trazabilidad.setPedidos(pedidos);
 			}
 		}
-		
+
 		List<EstadoTrazabilidad> estadoTrazabilidades = new ArrayList<EstadoTrazabilidad>();
 		Iterable<EstadoTrazabilidad> iterableEstadoTrazabilidad = estadoTrazabilidadRepository.findAll();
 		iterableEstadoTrazabilidad.forEach(estadoTrazabilidades::add);
-		
-		for (EstadoTrazabilidad estadoTrazabilidad : estadoTrazabilidades) { 
-			if(estadoTrazabilidad.getIdEstadoTrazabilidad() == trazabilidadRequest.getEstadoTrazabilidad().getIdEstadoTrazabilidad()) {
+
+		for (EstadoTrazabilidad estadoTrazabilidad : estadoTrazabilidades) {
+			if (estadoTrazabilidad.getIdEstadoTrazabilidad() == trazabilidadRequest.getEstadoTrazabilidad()
+					.getIdEstadoTrazabilidad()) {
 				trazabilidad.setEstadoTrazabilidad(estadoTrazabilidad);
 			}
 		}
-		
+
 		trazabilidadRepository.save(trazabilidad);
-		
-//Productos
+
+		//Trazabilidad
 		List<Trazabilidad> trazabilidades = new ArrayList<Trazabilidad>();
 		Iterable<Trazabilidad> iterableTrazabilidades = trazabilidadRepository.findAll();
 		iterableTrazabilidades.forEach(trazabilidades::add);
-		//set a productoResponse
+		// set a trazabilidadResponse
 		List<TrazabilidadResponse> response = new ArrayList<TrazabilidadResponse>();
-		for(Trazabilidad iTrazabilidad: trazabilidades) {
+		for (Trazabilidad iTrazabilidad : trazabilidades) {
 			TrazabilidadResponse trazabilidadResponse = new TrazabilidadResponse();
 			trazabilidadResponse.setIdTrazabilidad(iTrazabilidad.getIdTrazabilidad());
 			trazabilidadResponse.setFechaInicioPreparacion(iTrazabilidad.getFechaInicioPreparacion());
@@ -127,7 +127,7 @@ public class TrazabilidadServiceImpl implements TrazabilidadService{
 			trazabilidadResponse.setEstadoTrazabilidad(iTrazabilidad.getEstadoTrazabilidad());
 			response.add(trazabilidadResponse);
 		}
-		
+
 		responseServiceObject.setBody(response);
 
 		responseServiceMessage.setTimestamp(new Date());
@@ -150,9 +150,9 @@ public class TrazabilidadServiceImpl implements TrazabilidadService{
 		Trazabilidad trazabilidad = new Trazabilidad();
 		trazabilidad.setIdTrazabilidad(idTrazabilidad);
 		trazabilidad.setFechaInicioPreparacion(trazabilidadRequest.getFechaInicioPreparacion());
-		
+
 		responseServiceObject.setBody(trazabilidadRepository.save(trazabilidad));
-		
+
 		responseServiceMessage.setTimestamp(new Date());
 		responseServiceMessage.setCode("201");// 201 = create ok
 		responseServiceMessage.setType(ResponseServiceMessageType.OK);
@@ -181,7 +181,7 @@ public class TrazabilidadServiceImpl implements TrazabilidadService{
 		Iterable<Trazabilidad> iterableTrazabilidades = trazabilidadRepository.findAll();
 
 		iterableTrazabilidades.forEach(trazabilidades::add);
-		
+
 		responseServiceObject.setBody(trazabilidades);
 
 		responseServiceMessage.setTimestamp(new Date());
@@ -195,5 +195,5 @@ public class TrazabilidadServiceImpl implements TrazabilidadService{
 
 		return responseServiceObject;
 	}
-	
+
 }
