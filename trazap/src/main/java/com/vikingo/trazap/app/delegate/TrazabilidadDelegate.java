e25a10a09e47451074f2a6420fbd7e94c1d886ba
JaimeRG <=>
2022-05-10 14:32:18 -0400

    avances trazabilidad

--- trazap/src/main/java/com/vikingo/trazap/app/delegate/TrazabilidadDelegate.java
+++ trazap/src/main/java/com/vikingo/trazap/app/delegate/TrazabilidadDelegate.java
@@ -1,11 +1,14 @@
 package com.vikingo.trazap.app.delegate;
 
-//import com.vikingo.trazap.app.exceptions.ServiceException;
-import com.vikingo.trazap.app.repository.model.Trazabilidad;
+import com.vikingo.trazap.app.exceptions.ServiceException;
+import com.vikingo.trazap.app.service.request.TrazabilidadRequest;
 import com.vikingo.trazap.app.service.response.ResponseServiceObject;
 
 public interface TrazabilidadDelegate {
 
 	public ResponseServiceObject findAll();
-	public ResponseServiceObject save(Trazabilidad trazabilidad);
+	public ResponseServiceObject save(int idTrazabilidad, TrazabilidadRequest trazabilidadRequest);
+	public ResponseServiceObject save(TrazabilidadRequest trazabilidadRequest);
+	public ResponseServiceObject findByid(Integer idTrazabilidad) throws ServiceException;
+	public ResponseServiceObject deleteById(TrazabilidadRequest trazabilidadRequest);
 }
